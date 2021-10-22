f3209e77f95704e2726d829846007176880cd559
Eslam <eslamallam73@gmail.com>
2021-10-22 22:04:03 +0300

    using VGG-renet50 instead of VGG-imagenet, added 2 dense layers manually, implemented dropout and l1,l2 kernel regulaziers for improved accuracy, using 1 layer DCA instead of MDCA

--- AIFR-VGG.py
+++ AIFR-VGG.py
@@ -1,6 +1,5 @@
 #%%
 import tensorflow as tf
-from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.applications.vgg16 import preprocess_input
 from keras.models import Model
 from tensorflow.keras.callbacks import EarlyStopping
@@ -10,7 +9,7 @@ import graphviz
 from keras.utils.vis_utils import plot_model
 
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense,Dropout
 
 from dcaFuse import dcaFuse
 from keras import backend as K
@@ -20,6 +19,9 @@ import os
 import cv2 
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from keras_vggface.vggface import VGGFace
+from tensorflow.keras import regularizers
+
 
 
 
@@ -62,14 +64,19 @@ print(y_train.shape)
 
 
 #%%
-
-base_model = VGG16(weights="imagenet", include_top=True, input_shape=X_train[0].shape)
+base_model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
 base_model.trainable = False ## Not trainable weights
 
-model = Sequential()
-for layer in base_model.layers[:-1]: # go through until last layer
-    model.add(layer)
-model.add(Dense(82, activation='softmax'))
+
+x = base_model.layers[-1].output 
+dense1 = Dense(4096, activation = "relu",name='fc1',kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4))(x)
+dense2 = Dense(4096,activation='relu',name='fc2',kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4))(dense1)
+drop = Dropout(0.2)(dense2)
+predictions = Dense(82,activation='softmax')(drop)
+model = Model(inputs = base_model.input, outputs = predictions)
+
+
+
 
 model.summary()
 model.compile(
@@ -77,52 +84,62 @@ model.compile(
     loss='categorical_crossentropy',
     metrics=['accuracy'],
 )
-
+#%%
 es = EarlyStopping(monitor='val_accuracy', mode='max', patience=5,  restore_best_weights=True)
 
-model.fit(X_train, y_train, epochs=50,batch_size=56, callbacks=[es],validation_data=(X_test,y_test))
+model.fit(X_train, y_train, epochs=50,batch_size=32, callbacks=[es],validation_data=(X_test,y_test))
 
 
 
 #%%
 
 m1 = Model(inputs=model.input, outputs=model.get_layer('fc1').output)
-fc1 = m1.predict(X_train)
+fc1_train = m1.predict(X_train)
+fc1_test = m1.predict(X_test)
 
 m1 = Model(inputs=model.input, outputs=model.get_layer('fc2').output)
-fc2 = m1.predict(X_train)
+fc2_train = m1.predict(X_train)
+fc2_test = m1.predict(X_test)
 
-m1 = Model(inputs=model.input, outputs=model.get_layer('flatten').output)
-flatten = m1.predict(X_train)
+fc1_train = fc1_train.T
+fc2_train = fc2_train.T
 
-fc1 = fc1.T
-fc2 = fc2.T
-flatten = flatten.T
+fc1_test = fc1_test.T
+fc2_test = fc2_test.T
 #%%
-print("vector 1 shape :", flatten.shape)
-print("vector 2 shape :", fc1.shape)
-print("vector 3 shape :", fc2.shape)
+print("vector 1 shape :", fc1_train.shape)
+print("vector 2 shape :", fc2_train.shape)
 print("Labels shape: ", y_train1.shape)
 
 #%%
-Xs, Ys, Ax, Ay= dcaFuse(fc1,flatten,y_train1)
-fused_vector1 = np.add(Xs, Ys)
-
-print("fusion 1 Done")
-
-Xs, Ys, Ax, Ay= dcaFuse(fc1,fc2,y_train1)
-fused_vector2 = np.add(Xs, Ys)
-
-print("fusion 2 Done")
-
-Xs, Ys, Ax, Ay = dcaFuse(fused_vector1, fused_vector2, y_train1)
-fused_vector3 = np.add(Xs, Ys)
-
-print("fusion 3 Done")
 
 
-print("vector 1 shape: ",fused_vector1.shape)
-print("vector 2 shape: ",fused_vector2.shape)
-print("vector 3 shape: ",fused_vector3.shape)
+Xs, Ys, Ax, Ay= dcaFuse(fc1_train,fc2_train,y_train1)
+fused_vector = np.concatenate((Xs, Ys))
+
+testX = np.matmul(Ax,fc1_test)
+testY = np.matmul(Ay, fc2_test)
+test_vector = np.concatenate((testX,testY))
+
+print("fusion Done")
+print("fused_vector shape: ",fused_vector.shape)
+print("test_vector shape: ",test_vector.shape)
+
+#%%
+
+fused_vector = fused_vector.T
+test_vector = test_vector.T
+
+
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=1)
+classifier.fit(fused_vector,y_train1)
+predicted = classifier.predict(test_vector)
+
+from sklearn import metrics
+# Model Accuracy, how often is the classifier correct?
+print("Accuracy:",metrics.accuracy_score(y_test1, predicted))
+
+#check the dcaFuse mat file sample use cuz u fcked up dog
 
 # %%
