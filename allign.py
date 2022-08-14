8e7792aecadc478adce1bac2c179d9d170ec775d
eslamallam <eslamallam73@gmail.com>
2022-08-15 00:51:44 +0300

    fix: image allignment and preprocessing now uses mediaPipe for accurate results

--- allign.py
+++ allign.py
@@ -1,172 +1,138 @@
-# install and import above modules first
-import os
+# IMPORTING LIBRARIES
 import cv2
-import math
-import matplotlib.pyplot as pl
-import pandas as pd
-from PIL import Image
-import numpy as np
 import mediapipe as mp
-from mediapipe.python.solutions.drawing_utils import _normalized_to_pixel_coordinates as normalizeCoardinates
+import numpy as np
+import os
+from tqdm import tqdm as meter
 
 
-# Detect face
-def face_detection(img):
-	results = mp_face.process(img)
-	detection = results.detections[0]
-
-	eye_left=mp_face_detection.get_key_point(detection, mp_face_detection.FaceKeyPoint.LEFT_EYE)
-	eye_right=mp_face_detection.get_key_point(detection, mp_face_detection.FaceKeyPoint.RIGHT_EYE)
-
-	image_cols = img.shape[1]
-	image_rows = img.shape[0]
-	eye_right = normalizeCoardinates(eye_right.x,eye_right.y, image_cols,image_rows)
-	eye_left = normalizeCoardinates(eye_left.x,eye_left.y, image_cols,image_rows)
-
-	cv2.circle(img, eye_right, color=(255,0,0), radius=2, thickness=20)
-	cv2.circle(img, eye_left, color=(255,0,0), radius=2, thickness=20)
-
-	pl.imshow(img[:,:,::-1])
-	pl.show()
-
-	print(f'left eye: {eye_left}\nright eye: {eye_right}')
-	#img = img[top_left[1]:top_left[1]+hight, top_left[0]:top_left[0]+width]
-	return img, cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
 
 
-def trignometry_for_distance(a, b):
-	return math.sqrt(((b[0] - a[0]) * (b[0] - a[0])) +\
-					((b[1] - a[1]) * (b[1] - a[1])))
+# INITIALIZING OBJECTS
+mp_face_mesh = mp.solutions.face_mesh
 
-# Find eyes
-def Face_Alignment(img_path):
-	img_raw = cv2.imread(img_path).copy()
-	img, gray_img = face_detection(cv2.imread(img_path))
-	gray_img = process_image(gray_img)
-	eyes = eye_detector.detectMultiScale(gray_img)
-	
+FINAL_DIMENSIONS = (224, 224)
+DATASET_DIRECTORY = './datasets/FGNET/images'
+PROCESSED_IMAGE_DIRECTORY = './datasets/FGNET/new_images'
 
-	# for multiple people in an image find the largest
-	# pair of eyes
-	if len(eyes) >= 2:
-		eye = eyes[:, 2]
-		container1 = []
-		for i in range(0, len(eye)):
-			container = (eye[i], i)
-			container1.append(container)
-		df = pd.DataFrame(container1, columns=[
-						"length", "idx"]).sort_values(by=['length'])
-		eyes = eyes[df.idx.values[0:2]]
 
-		# deciding to choose left and right eye
-		eye_1 = eyes[0]
-		eye_2 = eyes[1]
-		if eye_1[0] > eye_2[0]:
-			left_eye = eye_2
-			right_eye = eye_1
-		else:
-			left_eye = eye_1
-			right_eye = eye_2
 
-		# center of eyes
-		# center of right eye
-		right_eye_center = (
-			int(right_eye[0] + (right_eye[2]/2)),
-		int(right_eye[1] + (right_eye[3]/2)))
-		right_eye_x = right_eye_center[0]
-		right_eye_y = right_eye_center[1]
-		cv2.circle(img, right_eye_center, 2, (255, 0, 0), 3)
 
-		# center of left eye
-		left_eye_center = (
-			int(left_eye[0] + (left_eye[2] / 2)),
-		int(left_eye[1] + (left_eye[3] / 2)))
-		left_eye_x = left_eye_center[0]
-		left_eye_y = left_eye_center[1]
-		cv2.circle(img, left_eye_center, 2, (255, 0, 0), 3)
-
-		# finding rotation direction
-		if left_eye_y > right_eye_y:
-			print("Rotate image to clock direction")
-			point_3rd = (right_eye_x, left_eye_y)
-			direction = -1 # rotate image direction to clock
-		else:
-			print("Rotate to inverse clock direction")
-			point_3rd = (left_eye_x, right_eye_y)
-			direction = 1 # rotate inverse direction of clock
-
-		cv2.circle(img, point_3rd, 2, (255, 0, 0), 2)
-		a = trignometry_for_distance(left_eye_center,
-									point_3rd)
-		b = trignometry_for_distance(right_eye_center,
-									point_3rd)
-		c = trignometry_for_distance(right_eye_center,
-									left_eye_center)
-		cos_a = (b*b + c*c - a*a)/(2*b*c)
-		angle = (np.arccos(cos_a) * 180) / math.pi
-
-		if direction == -1:
-			angle = 90 - angle
-		else:
-			angle = -(90-angle)
-
-		# rotate image
-		new_img = Image.fromarray(img_raw)
-		new_img = np.array(new_img.rotate(direction * angle))
-
-	return new_img
-
-def process_image(img):
-    #dim = (224, 224)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray = cv2.equalizeHist(gray)
-    #gray = cv2.resize(gray, dim, interpolation = cv2.INTER_AREA)
-    return gray
+def proccess_image(image):
+    # DETECT THE FACE LANDMARKS
+    with mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5, static_image_mode=True, max_num_faces=1, refine_landmarks=True) as face_mesh:
     
+        
+        shape = image.shape
+
+        # convert the color space from BGR to RGB
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        # To improve performance
+        image.flags.writeable = False
+
+        # Detect the face landmarks
+        results = face_mesh.process(image) 
 
 
-opencv_home = cv2.__file__
-folders = opencv_home.split(os.path.sep)[0:-1]
-path = folders[0]
-for folder in folders[1:]:
-	path = path + "/" + folder
-path_for_face = path+"/data/haarcascade_frontalface_alt2.xml"
-path_for_eyes = path+"/data/haarcascade_eye.xml"
-path_for_nose = path+"/data/haarcascade_mcs_nose.xml"
+        # To improve performance
+        image.flags.writeable = True
 
-modelFile = "models/face_detection_yunet_2022mar-act_int8-wt_int8-quantized.onnx"
-detector = cv2.FaceDetectorYN.create(
-        modelFile,
-        "",
-        (320, 320),
-        0.9,
-        0.3,
-        5000
-    )
-detector.setInputSize((224, 224))
+        # Convert back to the BGR color space
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-face_detector = cv2.CascadeClassifier(path_for_face)
-eye_detector = cv2.CascadeClassifier(path_for_eyes)
+        
+        face_landmarks = results.multi_face_landmarks[0]
 
-mp_face_detection = mp.solutions.face_detection
-mp_face = mp.solutions.face_detection.FaceDetection(
-    model_selection=1,  # model selection
-    min_detection_confidence=0.5  # confidence threshold
-)
+        left_eye_landmarks = [
+        face_landmarks.landmark[35] , face_landmarks.landmark[246], face_landmarks.landmark[161], face_landmarks.landmark[160],
+        face_landmarks.landmark[159], face_landmarks.landmark[158], face_landmarks.landmark[157], face_landmarks.landmark[173], 
+        face_landmarks.landmark[133], face_landmarks.landmark[155], face_landmarks.landmark[154], face_landmarks.landmark[153],
+        face_landmarks.landmark[145], face_landmarks.landmark[144], face_landmarks.landmark[163], face_landmarks.landmark[7] ]
+
+        x = [(p.x)*shape[1] for p in left_eye_landmarks]
+        y = [(p.y)*shape[0] for p in left_eye_landmarks]
+
+        left_eye = (sum(x) / len(left_eye_landmarks), sum(y) / len(left_eye_landmarks))
+        left_eye = [int(p) for p in left_eye]
+        left_eye_x = left_eye[0]
+        left_eye_y = left_eye[1]
+
+        right_eye_landmarks = [
+        face_landmarks.landmark[362], face_landmarks.landmark[398], face_landmarks.landmark[384], face_landmarks.landmark[385],
+        face_landmarks.landmark[386], face_landmarks.landmark[387], face_landmarks.landmark[388], face_landmarks.landmark[466], 
+        face_landmarks.landmark[263], face_landmarks.landmark[249], face_landmarks.landmark[390], face_landmarks.landmark[373],
+        face_landmarks.landmark[374], face_landmarks.landmark[380], face_landmarks.landmark[381], face_landmarks.landmark[382]]
+
+        x = [(p.x)*shape[1] for p in right_eye_landmarks]
+        y = [(p.y)*shape[0] for p in right_eye_landmarks]
+
+        right_eye = (sum(x) / len(right_eye_landmarks), sum(y) / len(right_eye_landmarks))
+        right_eye = [int(p) for p in right_eye]
+        right_eye_x = right_eye[0]
+        right_eye_y = right_eye[1]
+
+        delta_x = right_eye_x - left_eye_x
+        delta_y = right_eye_y - left_eye_y
+        angle=np.arctan(delta_y/delta_x)
+        angle = (angle * 180) / np.pi
+
+        # Width and height of the image
+        h, w = image.shape[:2]
+        # Calculating a center point of the image
+        # Integer division "//"" ensures that we receive whole numbers
+        center = (w // 2, h // 2)
+        # Defining a matrix M and calling
+        # cv2.getRotationMatrix2D method
+        M = cv2.getRotationMatrix2D(center, (angle), 1.0)
+        # Applying the rotation to our image using the
+        # cv2.warpAffine method
+        image = cv2.warpAffine(image, M, (w, h))
+
+        
+        # To improve performance
+        image.flags.writeable = False
+
+        results = face_mesh.process(image) 
+
+        # To improve performance
+        image.flags.writeable = True
+
+        face_landmarks = results.multi_face_landmarks[0] 
+
+        face_landmarks = [p for p in face_landmarks.landmark] 
+
+        x = [int(p.x * shape[1]) for p in face_landmarks]
+        y = [int(p.y * shape[0]) for p in face_landmarks]
+
+        mid_left = np.min(x)
+        mid_right = np.max(x)
+        mid_top = np.min(y)
+        mid_bot = np.max(y)
+
+        image = image[mid_top:mid_bot, mid_left:mid_right]
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.equalizeHist(image)
+        image = cv2.resize(image, FINAL_DIMENSIONS, interpolation = cv2.INTER_AREA)
+
+        return image
+
+
+images_list = os.listdir(DATASET_DIRECTORY)
+
+for image_name in meter(images_list):
+    path = DATASET_DIRECTORY+'/'+image_name
+    new_path = PROCESSED_IMAGE_DIRECTORY+'/'+image_name
+    image = cv2.imread(path).copy()
+    image = proccess_image(image)
+
+    if not os.path.exists(PROCESSED_IMAGE_DIRECTORY): os.mkdir(PROCESSED_IMAGE_DIRECTORY)
+        
+    cv2.imwrite(new_path, image)
 
 
 
-# Name of the image for face alignment if on
-# the other folder kindly paste the name of
-# the image with path included
-test_set = ["./datasets/FGNET/images/001A16.JPG"]
-for i in test_set:
-    alignedFace = Face_Alignment(i)
-    pl.imshow(alignedFace[:, :, ::-1])
-    pl.show()
-    img, gray_img = face_detection(alignedFace)
-    pl.imshow(img[:, :, ::-1])
-    pl.show()
-    img = process_image(img)
-    pl.imshow(img,cmap='gray')
-    pl.show()
+    '''cv2.imshow('image',image)
+    k = cv2.waitKey(0)
+    if k == ord('w'): continue
+    elif k == ord('q'): break'''
