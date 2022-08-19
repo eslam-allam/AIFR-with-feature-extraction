b568423bbfa8df4c89c79ac4653a12e2686fe081
Eslam Allam <86079760+eslam-allam@users.noreply.github.com>
2022-08-20 02:53:01 +0300

    feat: added two more options: dropout and variable KNN neighbors. various performance improvements using garbage collector

--- AIFR-VGG.py
+++ AIFR-VGG.py
@@ -5,7 +5,9 @@ parser.add_argument('-l','--loop', required=False, action='store_true', help='lo
 parser.add_argument('-es','--early-stop', required=False, action='store_true', help='stop the training early if the accuracy is not improving')
 parser.add_argument('-ne','--no-excel', required=False, action='store_true', help="don't save stats to excel files")
 parser.add_argument('-v','--variable-dropout', required=False, type=float, action='store', help="increase dropout after every iteration")
+parser.add_argument('-d','--dropout', required=False, type=float, action='store', help="Set dropout value")
 parser.add_argument('-s','--model-summary', required=False, action='store_true', help="Print summary of built TF model")
+parser.add_argument('-vk','--variable-knn', required=False, action='store_true', help="keep changing number of KNN neighbors until target accuracy is reached.")
 parser.add_argument('-bc','--bot-config', metavar='\b', required=False, help="Modify bot configuration file location.")
 parser.add_argument('-nt','--notify-telegram', required=False, action='store_true', help="send telegram notification when training is finished")
 parser.add_argument('-kn','--knn-neighbors', metavar='\b', required=False, type=int, help='number of KNN neighbors')
@@ -35,6 +37,8 @@ import requests
 import sys
 import re
 from tqdm import tqdm as meter
+import gc
+import random
 
 tf.get_logger().setLevel(logging.CRITICAL)
 mylogs = logging.getLogger(__name__)
@@ -58,9 +62,12 @@ mylogs.addHandler(file)
 def handle_exception(exc_type, exc_value, exc_traceback):
     if issubclass(exc_type, KeyboardInterrupt):
         mylogs.info('KEYBOARD INTERRUPT, PROGRAM TERMINATED')
-        sys.exit(0)
+    else:
+        mylogs.critical("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+        if args.notify_telegram: notify_telegram(config_from_file=True, message=f'Model training experienced a critical error and was terninated.\nError info:\nException type: {exc_type}\nException value: {exc_value}\nTraceback: {exc_traceback}')
+    sys.exit(0)
 
-    mylogs.critical("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+    
 
 sys.excepthook = handle_exception
 sys.setrecursionlimit(10000)
@@ -71,6 +78,8 @@ IMAGE_SHAPE = (224, 224, 3)
 MODEL_SAVE_DIRECTORY = './saved_models/'
 SAVE_MODEL_ACCURACY_THRESHOLD = 0.86
 BOT_CONFIG_PATH = './bot_config.txt'
+EPOCHS = 150
+DROPOUT = 0.2
 
 
 def lr_schedule(epoch):
@@ -83,6 +92,8 @@ def lr_schedule(epoch):
         lrate = 0.0001
     if epoch > 25:
         lrate = 0.00005
+    if epoch > 50:
+        lrate = 0.00001
     return lrate
 
 def load_dataset(directory=DATASET_DIRECTORY, image_shape=IMAGE_SHAPE):
@@ -110,12 +121,14 @@ def load_dataset(directory=DATASET_DIRECTORY, image_shape=IMAGE_SHAPE):
     
     num_classes = len(np.unique(labels))
     mylogs.debug(f'NUMBER OF CLASSES: {num_classes}')
+    
+    seed = random.randint(0, 2**32 - 1)
 
     # split the data into train and test
     x_train, x_test, y_train, y_test = train_test_split(
-        images_array, labels, test_size=0.20, random_state=33
+        images_array, labels, test_size=0.20, random_state=seed
     )
-    y_train_ages, y_test_ages = train_test_split(ages, test_size=0.20, random_state=33)
+    y_train_ages, y_test_ages = train_test_split(ages, test_size=0.20, random_state=seed)
     
 
     y_test = np.array(y_test)
@@ -133,7 +146,7 @@ def load_dataset(directory=DATASET_DIRECTORY, image_shape=IMAGE_SHAPE):
     
     return x_train, y_train, x_test, y_test, y_train_categorical, y_test_ages, num_classes
 
-def build_model(x_train, y_train,epochs=50, early_stop=True, variable_lr=True, batch_size=128, model_summary=False, num_classes=82, drop_out= 0.2):
+def build_model(x_train, y_train,epochs=EPOCHS, early_stop=True, variable_lr=True, batch_size=128, model_summary=False, num_classes=82, drop_out= DROPOUT):
 
     mylogs.info('CREATING MODEL')
 
@@ -306,7 +319,7 @@ def model_stats_to_excel(y_test_ages, predicted, history, y_test, output_directo
     mylogs.info(f"EXPORTING EXCEL FILE TO {output_directory+'/Model_accuracy_stats.xlsx'}")
     writer.save()
 
-def save_model(m1, m2, flatten, Ax1, Ax2, Ax3, Ay1, Ay2, Ay3, classifier, accuracy,save_directory=MODEL_SAVE_DIRECTORY, model_name=None, save_excel_stats=False, y_test_ages=None, predicted=None, history=None, y_test=None):
+def save_model(model, m1, m2, flatten, Ax1, Ax2, Ax3, Ay1, Ay2, Ay3, classifier, accuracy,save_directory=MODEL_SAVE_DIRECTORY, model_name=None, save_excel_stats=False, y_test_ages=None, predicted=None, history=None, y_test=None):
 
     if not model_name: 
         saved_models = os.listdir(save_directory)
@@ -314,7 +327,7 @@ def save_model(m1, m2, flatten, Ax1, Ax2, Ax3, Ay1, Ay2, Ay3, classifier, accura
 
     model_location = save_directory+model_name
     os.makedirs(model_location,exist_ok=False)
-    models = [m1, m2, flatten, Ax1, Ax2, Ax3, Ay1, Ay2, Ay3, classifier]
+    models = [model, m1, m2, flatten, Ax1, Ax2, Ax3, Ay1, Ay2, Ay3, classifier]
 
     mylogs.info(f"EXPORTING MODELS TO {model_location+'/compressed_models.pcl'}")
     with open(model_location+'/compressed_models.pcl', "wb") as f:
@@ -327,9 +340,9 @@ def save_model(m1, m2, flatten, Ax1, Ax2, Ax3, Ay1, Ay2, Ay3, classifier, accura
     if model_name: return model_name
     else: return 'UnnamedModel'
     
-def  notify_telegram(model_name=None, accuracy=None, telegram_bot_token=None, telegram_chatID=None, bot_config_file=BOT_CONFIG_PATH, init=False):
+def  notify_telegram(model_name=None, accuracy=None, telegram_bot_token=None, telegram_chatID=None, bot_config_file=BOT_CONFIG_PATH, init=False, config_from_file=False, message=''):
 
-    if init:
+    if init or config_from_file:
         if os.path.exists(bot_config_file):
             try:
                 with open(bot_config_file,'r', encoding='utf-8') as f:
@@ -357,58 +370,68 @@ def  notify_telegram(model_name=None, accuracy=None, telegram_bot_token=None, te
             
     if init: return  telegram_bot_token, telegram_chatID
 
-    assert model_name and accuracy, f"INVALID VALUES FOR MODEL_NAME, ACCURACY '{model_name}', '{accuracy}'"
-    response = requests.get('https://api.telegram.org/bot{}/sendMessage'.format(telegram_bot_token),params={'text':f'Your model "{model_name}" has finished training with an accracy of: {accuracy*100:.2f}%','chat_id': '{}'.format(telegram_chatID)})
+    
+    if not message: 
+        assert model_name and accuracy, f"INVALID VALUES FOR MODEL_NAME, ACCURACY '{model_name}', '{accuracy}'"
+        message = f'Your model "{model_name}" has finished training with an accracy of: {accuracy*100:.2f}%'
+    response = requests.get('https://api.telegram.org/bot{}/sendMessage'.format(telegram_bot_token),params={'text':message ,'chat_id': '{}'.format(telegram_chatID)})
     status = response.json()
     if status['ok']:
         mylogs.info('**********************STATUS:OK**********************')
         sender = status['result']['from']
         chat = status['result']['chat']
         mylogs.info('MESSAGE SENT THROUGH {} TO {} {}'.format(sender['first_name'],chat['first_name'], chat['last_name']))
-        mylogs.info('MESSAGE: {}'.format(f'Your model "{model_name}" has finished training with an accracy of: {accuracy*100:.2f}%'))
+        if model_name: mylogs.info('MESSAGE: {}'.format(f'Your model "{model_name}" has finished training with an accracy of: {accuracy*100:.2f}%'))
     else:
         mylogs.warning('**********************STATUS:NO OK**********************')
         mylogs.warning('MESSAGE NOT SENT!! PLEASE CHECK BOT PARAMETERS OR CHAT ID')
 
-def main(loop=False ,early_stop=False, save_excel_stats=True,KNN_neighbors=5, save_directory=MODEL_SAVE_DIRECTORY, accuracy_threshold=SAVE_MODEL_ACCURACY_THRESHOLD, model_summary=False, drop_out=0.2, variable_dropout=None):
+def main(loop=False ,early_stop=False, save_excel_stats=True,KNN_neighbors=5, save_directory=MODEL_SAVE_DIRECTORY, accuracy_threshold=SAVE_MODEL_ACCURACY_THRESHOLD, model_summary=False, drop_out=DROPOUT, variable_dropout=None, variable_knn=False):
     x_train, y_train, x_test, y_test, y_train_categorical, y_test_ages, num_classes = load_dataset()
 
     while True:
         model, history = build_model(x_train, y_train_categorical, early_stop=early_stop, model_summary=model_summary, num_classes=num_classes, drop_out=drop_out)
         fused_vector, test_vector, m1, m2, flatten, Ax1, Ax2, Ax3, Ay1, Ay2, Ay3 = three_layer_MDCA(x_train, x_test,y_train, model)
+        
+        for i in range(KNN_neighbors,KNN_neighbors+10):
+            mylogs.info(f'NUMBER OF kNN NEIGHBORS = {KNN_neighbors}')
+            classifier = KNeighborsClassifier(n_neighbors=i)
+            classifier.fit(fused_vector, y_train)
 
-        classifier = KNeighborsClassifier(n_neighbors=KNN_neighbors)
-        classifier.fit(fused_vector, y_train)
+            # predict and display using DNN and KNN classifiers
+            predicted = np.argmax(model.predict(x_test), axis=-1)
+            mylogs.info("DNN Accuracy: {}".format(metrics.accuracy_score(y_test, predicted)))
 
-        # predict and display using DNN and KNN classifiers
-        predicted = np.argmax(model.predict(x_test), axis=-1)
-        mylogs.info("DNN Accuracy: {}".format(metrics.accuracy_score(y_test, predicted)))
+            predicted = classifier.predict(test_vector)
+            mylogs.info("DCA Accuracy: {}".format(metrics.accuracy_score(y_test, predicted)))
 
-        predicted = classifier.predict(test_vector)
-        mylogs.info("DCA Accuracy: {}".format(metrics.accuracy_score(y_test, predicted)))
+            DCA_accuracy = metrics.accuracy_score(y_test, predicted)
 
-        DCA_accuracy = metrics.accuracy_score(y_test, predicted)
-
-        if DCA_accuracy >= accuracy_threshold:
-            model_name = save_model(m1, m2, flatten, Ax1, Ax2, Ax3, Ay1, Ay2, Ay3, classifier, DCA_accuracy, 
-            save_excel_stats=save_excel_stats, y_test_ages=y_test_ages, predicted=predicted, history=history, y_test=y_test, save_directory=save_directory)
-            break
-        else: model_name = 'Unnamed_model'
+            if DCA_accuracy >= accuracy_threshold:
+                model_name = save_model(model, m1, m2, flatten, Ax1, Ax2, Ax3, Ay1, Ay2, Ay3, classifier, DCA_accuracy,
+                save_excel_stats=save_excel_stats, y_test_ages=y_test_ages, predicted=predicted, history=history, y_test=y_test, save_directory=save_directory)
+                return model_name, DCA_accuracy
+            
+            else: model_name = 'Unnamed_model'
+            if not variable_knn: break
 
         tf.keras.backend.set_learning_phase(1)
         tf.keras.backend.clear_session()
-        del model, history, fused_vector, test_vector, m1, m2, flatten, Ax1, Ax2, Ax3, Ay1, Ay2, Ay3,
+        del model, history, fused_vector, test_vector, m1, m2, flatten, Ax1, Ax2, Ax3, Ay1, Ay2, Ay3
         classifier, predicted
         if not loop: break
         elif variable_dropout is not None: 
             drop_out += variable_dropout
             mylogs.info(f'VARIABLE DROPOUT ENABLED, CURRENT DROPOUT = {drop_out}')
-            assert drop_out <= 0.6, 'DROP OUT TOO HIGH, ABORTING' 
+            assert drop_out <= 0.6, 'DROP OUT TOO HIGH, ABORTING'
+        
+        gc.collect(0)
+        gc.collect() 
     return model_name, DCA_accuracy
 
 
         
-loop, early_stop, save_excel_stats, KNN_neighbors, save_directory, accuracy_threshold, model_summary, variable_dropout = False ,False, True, 5, MODEL_SAVE_DIRECTORY, SAVE_MODEL_ACCURACY_THRESHOLD, False, None
+loop, early_stop, save_excel_stats, KNN_neighbors, save_directory, accuracy_threshold, model_summary, variable_dropout, drop_out, variable_knn = False ,False, True, 5, MODEL_SAVE_DIRECTORY, SAVE_MODEL_ACCURACY_THRESHOLD, False, None, DROPOUT, False
 if args.early_stop:
     early_stop = True
 if args.no_excel:
@@ -423,27 +446,31 @@ if args.loop:
     loop = True
 if args.variable_dropout:
     variable_dropout = args.variable_dropout
+if args.variable_knn:
+    variable_knn = True
+if args.dropout:
+    drop_out = args.dropout
 if args.accuracy_threshold:
     accuracy_threshold = args.accuracy_threshold
     
 mylogs.info(f'''STARTING TRAINING WITH THE FOLLOWING CONFIGURATION:
-                             LOOP = {loop}, EARLY_STOP = {early_stop}, SAVE_EXCEL_STATS = {save_excel_stats},
+                             LOOP = {loop}, EARLY_STOP = {early_stop}, SAVE_EXCEL_STATS = {save_excel_stats}, MODEL_SUMMARY = {model_summary}
                              NUMBER_OF_KNN_NEIGHBOURS = {KNN_neighbors}
-                             SAVE_DIRECTORY = {save_directory}
                              ACCURACY_THRESHOLD = {accuracy_threshold}
-                             VARIABLE_DROPOUT = {variable_dropout} 
-----------------------------------------------------------------------''')
+                             DROPOUT = {drop_out}
+                             VARIABLE_DROPOUT = {variable_dropout}
+                             KNN_NEIGHBORS = {KNN_neighbors}
+                             VARIABLE_KNN_NEIGHBORS = {variable_knn}
+                             SAVE_DIRECTORY = {save_directory}
+------------------------------------------------------------------------------------------------------------------------------------------------''')
 
 if args.notify_telegram: 
     mylogs.info('TELEGRAM NOTIFICATION ENABLED BY USER. STARTING CONFIG.')
     telegram_bot_token, telegram_chatID = notify_telegram(init=True)
 
-model_name, accuracy = main(loop, early_stop, save_excel_stats, KNN_neighbors, save_directory, accuracy_threshold, model_summary=model_summary, variable_dropout=variable_dropout)
+model_name, accuracy = main(loop, early_stop, save_excel_stats, KNN_neighbors, save_directory, accuracy_threshold, model_summary=model_summary, variable_dropout=variable_dropout, drop_out=drop_out, variable_knn=variable_knn)
 
 if args.notify_telegram: notify_telegram(model_name, accuracy, telegram_bot_token=telegram_bot_token, telegram_chatID=telegram_chatID)
-
-
-    
 
     
 
